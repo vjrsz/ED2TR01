adb76e068f278d8a92cc9f51c4e45da5498acc39
vjrsz <juniornunes1008@gmail.com>
2023-04-07 12:23:01 -0300

    add docs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*
+ * https://chat.openai.com/chat/b6da74e8-aee4-4861-a90e-74d94bc5b332 - Valter
+ * ESCOLHER DATASET:
+ * 1. https://www.kaggle.com/datasets?search=stock+management
+ * 2. https://www.kaggle.com/datasets?search=inventory+management
+ *
+ * Pré-processamento dos dados: Realizar pré-processamento dos dados coletados, como
+ * remoção de valores ausentes, normalização, etc
+ *
+ * https://www.youtube.com/watch?v=RP-4VMQHOTw&list=PLTLAlheiUm5FRR5BNn4iBFwzYHiNq2Iv2&index=17 Comparação de algoritmos de ordenação
+ */
+
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
