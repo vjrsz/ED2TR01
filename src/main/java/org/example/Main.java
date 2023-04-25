a41ceefc9a7d1fdaaa3ea84308325c32dd00414d
pedayako <francapedro68@gmail.com>
2023-04-25 20:46:44 -0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String csvFile = "C:\\xampp\\htdocs\\ED2\\EDDOIS\\data\\customer_segmentation.csv";
+        String csvFile = "data\\customer_segmentation.csv";
         List<String[]> data = CSVReaderFile.read(csvFile); // ler csv
         Segmentation[] segmentations = SegmentationService.create(data); // cria instancias
 
